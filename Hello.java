450cc0c8cc6bde94908e040c1336482e20d76f61
gillianfanning <112016380+gillianfanning@users.noreply.github.com>
2022-08-24 14:48:24 -0400

    Add files via upload
    Hello.java is the program we created to go along with Lab 1

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***********************************************************************
+*Java Program 1
+***********************************************************************
+*Gillian Fanning
+* 08/23/2022
+*CMSC 255 001 
+************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	System.out.println("Gillian");
+	System.out.println("Java Program Lab 1");
+	System.out.println("How to Download Java and Use It");
+	System.out.println("08/23/2022");
+	System.out.println("CMSC 255 001");
+	System.out.println();
+	}
+}
